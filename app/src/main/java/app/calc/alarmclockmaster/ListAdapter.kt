314fbe8b0f59093942994233ffaa5fa2e6986371
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-10-30 21:25:14 +0500

    AlarmClockMaster

---
+++ app/src/main/java/app/calc/alarmclockmaster/ListAdapter.kt
@@ -0,0 +1,45 @@
+package app.calc.alarmmaster.adapters
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.annotation.RequiresApi
+import app.calc.alarmclockmaster.R
+import app.calc.alarmmaster.ListData
+
+@Suppress("NAME_SHADOWING")
+class ListAdapter(private val context:Activity, private val list:ArrayList<ListData>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return list.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return list[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("ViewHolder", "UseSwitchCompatOrMaterialCode", "InflateParams", "MissingInflatedId")
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.list_view
+            , null, true)
+
+        val clock = rowView.findViewById(R.id.textClock) as TextView
+        val comment = rowView.findViewById(R.id.comment) as TextView
+        val switchs = rowView.findViewById(R.id.switch1) as Switch
+
+        clock.text = list[position].Clock
+        comment.text = list[position].comment
+        switchs.text = ""
+
+        return rowView
+    }
+}
---
+++ app/src/main/java/app/calc/alarmclockmaster/ListData.kt
@@ -0,0 +1,7 @@
+package app.calc.alarmmaster
+
+data class ListData(
+    var Clock: String,
+    var comment: String,
+    var switchs: String
+    )
