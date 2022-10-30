7013e6da04ff3007dd2e7616e96fc158d761c78b
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-10-30 19:32:18 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/RecoveryEmailCode.kt
+++ app/src/main/java/app/calc/alarmclockmaster/RecoveryEmailCode.kt
@@ -1,19 +1,28 @@
 package app.calc.alarmclockmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class RecoveryEmailCode : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_recovery_password)
+        setContentView(R.layout.activity_recovery_email_code)
 
         val viewRecoveryBack = findViewById<View>(R.id.viewRecoveryBack)
+        val btnRecovery = findViewById<Button>(R.id.btnRecovery)
 
         viewRecoveryBack.setOnClickListener {
             onBackPressed()
+        }
+
+        btnRecovery.setOnClickListener {
+            val intent = Intent(this, RecoveryPassword::class.java)
+            startActivity(intent)
+            finishAffinity()
         }
 
     }
--- app/src/main/java/app/calc/alarmclockmaster/RecoveryPassword.kt
+++ app/src/main/java/app/calc/alarmclockmaster/RecoveryPassword.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class RecoveryPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recovery_password2)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_recovery_password)
     }
 }
