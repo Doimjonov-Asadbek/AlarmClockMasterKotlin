ef97bd07a0bb3c387d8cc4f15835b93b03c5150c
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-11-02 15:27:37 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
+++ app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
@@ -100,6 +100,7 @@ class SignInActivity : AppCompatActivity() {
 
 
 
+
                             else{
                                 if(error == "email is not verified"){
                                     AlertDialog.Builder(this@SignInActivity)
