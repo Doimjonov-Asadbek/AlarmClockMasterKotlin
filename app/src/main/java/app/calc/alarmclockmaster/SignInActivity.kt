cf8ac084ce275ed06b28252c62c90bfe0089a977
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-11-05 23:30:41 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
+++ app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
@@ -77,8 +77,7 @@ class SignInActivity : AppCompatActivity() {
                                 startActivity(intent)
                                 finish()
                             }
-                        }
-                        
+                        }\
                         if (response.code() == 400){
                             json = gson.fromJson(response.errorBody()?.charStream(), JsonObject::class.java)
                             val error = json?.get("error")?.asString
