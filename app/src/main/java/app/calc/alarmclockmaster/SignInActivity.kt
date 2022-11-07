75c60ed5d2ef745414ae4afc12c9d1b076171422
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-11-08 00:43:15 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
+++ app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
@@ -1,6 +1,36 @@
     package app.calc.alarmclockmaster
 
-  
+    import android.content.Intent
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.ProgressBar
+    import android.widget.TextView
+    import android.widget.Toast
+    import androidx.appcompat.app.AlertDialog
+    import app.calc.alarmclockmaster.models.ApiClient
+    import app.calc.alarmclockmaster.models.SignIn
+    import com.google.gson.Gson
+    import com.google.gson.JsonObject
+    import retrofit2.Call
+
+    class SignInActivity : AppCompatActivity() {
+
+        var json: JsonObject? = null
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            supportActionBar?.hide()
+            setContentView(R.layout.activity_sign_in)
+
+            val txtSignRecoveryPassword = findViewById<TextView>(R.id.txtSignRecoveryPassword)
+            val txtSignRegistration = findViewById<TextView>(R.id.txtSignRegistration)
+            val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+            val edtSignEmail = findViewById<EditText>(R.id.edtSignEmail)
+            val edtSignPassword = findViewById<EditText>(R.id.edtSignPassword)
+            val gson = Gson()
+            val signProgress = findViewById<ProgressBar>(R.id.signProgress)
 
             txtSignRecoveryPassword.setOnClickListener {
                 val intent = Intent(this, RecoveryEmailCode::class.java)
