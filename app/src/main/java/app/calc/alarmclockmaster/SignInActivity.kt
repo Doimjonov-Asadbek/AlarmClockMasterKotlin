2c9bddcf9e97b865a7ccfd9d076bb136e21efac7
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-11-07 22:19:57 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
+++ app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
@@ -90,7 +90,6 @@ class SignInActivity : AppCompatActivity() {
                                 edtSignPassword?.error = "Parol noto'g'ri"
                             }
 
-
                             
                             if(error == "email is incorrect"){
                                 edtSignEmail?.error = "Bunday pochta mavjud emas "
