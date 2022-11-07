4007700edb3c7f282dbeffaaea43dd399064746b
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-11-08 00:53:39 +0500

    AlarmClockMaster

--- app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
+++ app/src/main/java/app/calc/alarmclockmaster/SignInActivity.kt
@@ -53,7 +53,71 @@
                 else if (edtSignPassword.text.toString().length < 4){
                     edtSignPassword.error = "Parol kamida 4 ta belgidan iborat bo'lishi kerak"
                 }
-           
+                else {
+
+                    signProgress.visibility = ProgressBar.VISIBLE
+                    btnSignIn.visibility = Button.INVISIBLE
+
+                    val signin = SignIn(
+                        edtSignEmail.text.toString(),
+                        edtSignPassword.text.toString()
+                    )
+
+                    val signIn: Call<SignIn> = ApiClient.userService.signIn(signin)
+                    signIn.enqueue(object : retrofit2.Callback<SignIn> {
+                        override fun onResponse(call: Call<SignIn>, response: retrofit2.Response<SignIn>) {
+                            if (response.code() == 200){
+
+                                signProgress.visibility = ProgressBar.INVISIBLE
+                                btnSignIn.visibility = Button.VISIBLE
+
+                                val user = response.body()
+                                if (user != null){
+                                    val intent = Intent(this@SignInActivity,ClockActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                            }
+
+                            if (response.code() == 400){
+                                json = gson.fromJson(response.errorBody()?.charStream(), JsonObject::class.java)
+                                val error = json?.get("error")?.asString
+
+                                signProgress.visibility = ProgressBar.INVISIBLE
+                                btnSignIn.visibility = Button.VISIBLE
+
+                                if (error == "password is incorrect"){
+                                    edtSignPassword?.error = "Parol noto'g'ri"
+                                }
+                                if(error == "email is incorrect"){
+                                    edtSignEmail?.error = "Bunday pochta mavjud emas "
+                                } else{
+                                    if(error == "email is not verified"){
+                                        AlertDialog.Builder(this@SignInActivity)
+                                            .setTitle("Hisobingizni tasdiqlang !")
+                                            .setMessage("Hisobingiz tasdiqlanmagan. Tasdiqlash uchun kodni kiriting")
+                                            .setPositiveButton("OK"){dialog, which ->
+                                                dialog.dismiss()
+                                            }
+                                            .show()
+                                    }
+                                }
+                            }
+                            if (response.code() == 401){
+
+                                signProgress.visibility = ProgressBar.INVISIBLE
+                                btnSignIn.visibility = Button.VISIBLE
+
+                                json = gson.fromJson(response.errorBody()?.charStream(), JsonObject::class.java)
+                                val error = json?.get("error")?.asString
+                                if (error == "user is blocked") {
+                                    AlertDialog.Builder(this@SignInActivity)
+                                        .setTitle("Hisobingizni Bloknagan !")
+                                        .setMessage("Hisobingiz bloklangan. Iltimos Administrator bilan bog'laning !")
+                                        .setPositiveButton("OK") { dialog, which ->
+                                            dialog.dismiss()
+                                        }
+                                        .show()
                                 }
                             }
                         }
